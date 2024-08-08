7d10a36dd7f61623165533b9eadeb0284fc7f49e
Hilman <hpanser@gmail.com>
2024-08-08 18:00:50 +0700

    Create 3 Email Sender.py

---
+++ 3 Email sender.py
@@ -0,0 +1,23 @@
+from email.message import EmailMessage
+import ssl
+import smtplib
+
+email_sender = "" #Fill with sender email
+email_password = "" #Fill with password sender email
+
+email_receiver = ""
+
+subject = "Test"
+body = "" "Test body email" ""
+
+em = EmailMessage()
+em['From'] = email_sender
+em['To'] = email_receiver
+em['Subject'] = subject
+em.set_content(body)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    smtp.login(email_sender, email_password)
+    smtp.sendmail(email_sender, email_receiver, em.as_string())
