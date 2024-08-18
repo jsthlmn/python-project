974e218b328387ee1451c0dffad4a385873bd7e5
Hilman <hpanser@gmail.com>
2024-08-18 11:33:33 +0700

    Create 20 web scraper.py

---
+++ 20 web scraper.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.codewithtomi.ml/'
+
+r = requests.get(url)
+
+soup = BeautifulSoup(r.content, 'lxml')
+title = soup.find_all('h2', {'class':'post-title'})
+print(title)
+
+title[0].getText()
+title1 = title[0].getText()
+print(title1)
+
+for t in title:
+    print(t.getText())
