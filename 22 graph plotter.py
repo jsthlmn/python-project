f34ea7c8077fa9db65c239aa4f8874dc692ad20a
Hilman <hpanser@gmail.com>
2024-08-19 11:25:37 +0700

    Create 22 graph plotter.py

---
+++ 22 graph plotter.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+x1 = [2, 4, 5]
+y1 = [2, 3, 6]
+
+plt.plot(x1, y1, label = 'Line 1')
+
+x2 = [2, 4, 5]
+y2 = [2, 3, 6]
+
+plt.plot(x2, y2, label = 'Line 2')
+
+plt.xlabel('X Axis')
+plt.ylabel('Y Axis')
+
+plt.title('Demo Graph - Two Lines')
+
+plt.legend()
+
+plt.show
