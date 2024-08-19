0f96456d4c1a720cd390ffb98014dcfaf2d33ce9
Hilman <hpanser@gmail.com>
2024-08-19 11:26:13 +0700

    Create 23 image editor.py

---
+++ 23 image editor.py
@@ -0,0 +1,15 @@
+from PIL import Image, ImageEnhance, ImageFilter
+import os
+
+path = './imgs'
+pathOut = '/editedImgs'
+
+for filename in os.listdir(path):
+    img = Image.open(f"{path}/{filename}")
+    edit = img.filter(ImageFilter.SHARPEN).rotate(-90)
+    factor = 1.5
+    enhacer = ImageEnhance.Contrast(edit)
+    edit = enhacer.enhance(factor)
+
+    clean_name = os.path.splitext(filename) [0]
+    edit.save(f".{pathOut}/{clean_name}_edited.jpg")
